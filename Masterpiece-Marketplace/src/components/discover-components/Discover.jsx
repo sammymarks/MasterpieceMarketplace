017f9c5fdca325f369bc7f378dddceb9362b06cd
sammymarks <sammy.marks@gmail.com>
2023-11-06 12:45:36 -0600

    discover progress

--- Masterpiece-Marketplace/src/components/discover-components/Discover.jsx
+++ Masterpiece-Marketplace/src/components/discover-components/Discover.jsx
@@ -8,21 +8,57 @@ import AuctionSearchResults from './AuctionsSearchResults'
 
 
 export default function Discover () {
+    
+
+    const tempArray = [
+        {
+            artist: "BlenvanGogh",
+            title: "Arles Sunflower Initial",
+            description: "Sunflowers",
+            genre: "Post-Impressionist",
+            material: ["Oil", "Canvas"],
+            creationYear: "1988",
+            imageURLs: ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Vincent_Van_Gogh_-_Three_Sunflowers_F453.jpg/1280px-Vincent_Van_Gogh_-_Three_Sunflowers_F453.jpg"]
+        },
+        {
+            artist: "BlenvanGogh",
+            title: "Arles Sunflower Initial",
+            description: "Sunflowers",
+            genre: "Painting",
+            material: ["Oil", "Canvas"],
+            creationYear: "1988",
+            imageURLs: ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Vincent_Van_Gogh_-_Three_Sunflowers_F453.jpg/1280px-Vincent_Van_Gogh_-_Three_Sunflowers_F453.jpg"]
+        },
+        {
+            artist: "BlenvanGogh",
+            title: "Arles Sunflower The Berceuse-Triptych",
+            description: "Sunflowers",
+            genre: "Painting",
+            material: ["Oil", "Canvas"],
+            creationYear: "1989",
+            imageURLs: ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Vincent_Willem_van_Gogh_128.jpg/1280px-Vincent_Willem_van_Gogh_128.jpg"]
+        }
+    ]
+
     const [searchCategory, setSearchCategory] = useState('none')
     const [searchText, setSearchText] = useState('')
     const [searchResultsMessage, setSearchResultsMessage] = useState('')
-    const [searchResultsArray, setSearchResultsArray] = useState([{}])
+    const [searchResultsArray, setSearchResultsArray] = useState(tempArray)
     
     const searchFunction = async () => {
         switch(searchCategory) {
             case "artists":
-                setSearchResultsArray([{results: "artists API Pull"}])
+                setSearchResultsArray(tempArray)
+                console.log("artists")
             break;
             case "artwork":
-                setSearchResultsArray([{results: "artwork API Pull"}])
+                setSearchResultsArray(tempArray)
+                console.log("artwork")
             break;
             case "auctions":
-                setSearchResultsArray([{results: "auctions API Pull"}])
+                setSearchResultsArray(tempArray)
+                console.log("auctions")
+
             break;
         }
     }
@@ -78,7 +114,17 @@ export default function Discover () {
             </div>
             <div className='search-results-message'>{searchResultsMessage}</div>
             <div className='search-results-container'>
-                {/* {searchResultsArray} */}
+                {searchResultsArray.map((item, index) => (
+                    <div 
+                        className='search-results-grid-item' 
+                        key={index} 
+                        // onClick={() => goToGridItem(index)}
+                    >
+                        <img className='detail-image' src={item.imageURLs}></img>
+                        <h2 className='detail-title'>{item.title}</h2>
+                        <h2 className='detail-artist'>{item.artist}</h2>
+                    </div>
+                ))}
             </div>
             {/* <ArtistSearchResults />
             <ArtworkSearchResults />
