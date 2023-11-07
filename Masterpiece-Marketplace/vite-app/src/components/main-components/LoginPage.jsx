be8f4a633679519b3aa9898fb93fe112905d18dd
Tahmid <tahmidsheikh610@gmail.com>
2023-11-07 08:46:08 -0500

    home/login/signup

--- Masterpiece-Marketplace/vite-app/src/components/main-components/LoginPage.jsx
+++ Masterpiece-Marketplace/vite-app/src/components/main-components/LoginPage.jsx
@@ -5,7 +5,43 @@ import axios from 'axios'
 
 export default function LoginPage () {
 
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = () => {
+    // login, req to server
+
+    // success, redirect to another page
+
+  }
+
+
     return (
-        <div className='LoginPage'> I am LoginPage </div>
+        <div className='LoginPage'>
+      <h2>Login</h2>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={handleLogin}>Login</button>
+      <p>
+        Don't have an account?{' '}
+        <Link to="/signup">Click here to make one.</Link>
+      </p>
+    </div>
     )
 }
--- Masterpiece-Marketplace/vite-app/src/components/main-components/SignUpPage.jsx
+++ Masterpiece-Marketplace/vite-app/src/components/main-components/SignUpPage.jsx
@@ -4,8 +4,81 @@ import axios from 'axios'
 
 
 export default function SignUpPage () {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [homeAddress, setHomeAddress] = useState('');
+    const [isArtist, setIsArtist] = useState(false);
+    const [artistDescription, setArtistDescription] = useState('');
+
+    const handleSignUp = () => {
+   
+// sign up logic
+
+
+  }
+
 
     return (
-        <div className='SignUpPage'> I am SignUpPage </div>
+        <div className='SignUpPage'>
+      <h2>Sign Up</h2>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="homeAddress">Home Address:</label>
+        <input
+          type="text"
+          id="homeAddress"
+          value={homeAddress}
+          onChange={(e) => setHomeAddress(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Are you an Artist?</label>
+        <input
+          type="checkbox"
+          id="isArtist"
+          checked={isArtist}
+          onChange={() => setIsArtist(!isArtist)}
+        />
+      </div>
+      {isArtist && (
+        <div>
+          <label htmlFor="artistDescription">Artist Description:</label>
+          <input
+            type="text"
+            id="artistDescription"
+            value={artistDescription}
+            onChange={(e) => setArtistDescription(e.target.value)}
+          />
+        </div>
+      )}
+      <button onClick={handleSignUp}>Sign Up</button>
+    </div>
     )
 }
