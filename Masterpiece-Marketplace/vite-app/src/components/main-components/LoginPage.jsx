68ca5b362ab922f6c7968b9659f8366ee235f2df
Tahmid <tahmidsheikh610@gmail.com>
2023-11-09 10:30:40 -0500

    login

--- Masterpiece-Marketplace/vite-app/src/components/main-components/LoginPage.jsx
+++ Masterpiece-Marketplace/vite-app/src/components/main-components/LoginPage.jsx
@@ -1,56 +1,74 @@
 import { useState, useEffect, useContext } from 'react'
-import { Link, useParams, useNavigate } from "react-router-dom"
+import { Link, useParams, useNavigate, redirect } from "react-router-dom"
 import axios from 'axios'
-import { useUserContext } from '../../App';
-import { BASE_DB_URL } from '../../globals';
+import { useUserContext } from '../../App'
+import { BASE_DB_URL } from '../../globals'
 
 
-export default function LoginPage () {
+export default function LoginPage() {
 
-    const { loggedInUser, setLoggedInUser, userArtwork, userAuctions, userBids } = useUserContext();
-    const navigate = useNavigate();
-
+    const { loggedInUser, setLoggedInUser, userArtwork, userAuctions, userBids } = useUserContext()
+    const navigate = useNavigate()
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
     const handleLogin = async () => {
-      // login, req to server
-      const url = `${BASE_DB_URL}users/654c41b45250b40dc2a87af2`
-      const response = await axios.get(url)
-      setLoggedInUser(response.data)
-      navigate('/discover')
-      // success, redirect to another page
+        try {
+            const response = await axios.get('http://localhost:3001/users')
+            const users = response.data
+
+            // check if there is a user with the provided username and password
+            const loggedInUser = users.find(
+                (user) => user.username === username && user.password === password
+            )
+
+            if (loggedInUser) {
+                setLoggedInUser(loggedInUser)
+                console.log(loggedInUser)
+                navigate('/discover')
+
+
+
+
+
+            } else {
+
+                console.error('Invalid username or password')
+                alert('Invalid username or password')
+            }
+        } catch (error) {
+            console.error('Error during login:', error)
+        }
     }
 
-    console.log(loggedInUser)
 
     return (
         <div className='LoginPage'>
-      <h2>Login</h2>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button onClick={handleLogin}>Login</button>
-      <p>
-        Don't have an account?{' '}
-        <Link to="/signup">Click here to make one.</Link>
-      </p>
-    </div>
+            <h2>Login</h2>
+            <div>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <button onClick={handleLogin}>Login</button>
+            <p>
+                Don't have an account?{' '}
+                <Link to="/signup">Click here to make one.</Link>
+            </p>
+        </div>
     )
 }
